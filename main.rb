d5de9931c839b4b94f3f1c84e2ef980368e2aa34
erinszabo <55733370+erinszabo@users.noreply.github.com>
2021-09-29 15:21:25 -0700

    remove comment mess,  break down functions
    too many comments with unclear variable names, code duplication, all in one function
    
    separate reverse and borg functions, better variable names, remove unneeded comments

--- main.rb
+++ main.rb
@@ -1,35 +1,20 @@
-# Name Mangler
-
-@name = "Johanna Jackson"
-
-# Mangle the name by reversing it or borgifying it
-def modified_name(choose)
-  # Split the name into first and last in an array
-  split_name = @name.split
-  #reversed_twice = split_name.reverse.reverse
-  #mangled_name = reversed_twice << "Mangled"
-  #return mangled_name * 2
-  # Reverse the array of name components so the array is [last, first]
-  reversed_name_components = split_name.reverse
-  # Rejoin the array of name components
-  new_name1 = reversed_name_components.join(' ')
-  # Store the mangled name
-  @name = new_name1
-  puts new_name1
-  if choose
-    # Split the name into last and first in an array
-    split_name2 = new_name1.split
-    # Reverse the array of name components so the array is [last, first]
-    reversed_name_components2 = split_name2.reverse
-    # Add borg to the end of the array
-    reversed_name_components2 << "Borg"
-    # Rejoin the array of name components
-    new_name2 = reversed_name_components2.join(' ')
-    puts new_name2
-    @name = new_name2
+def mangle_name(name, borg_bool)
+  name = reverse_name(name)
+  if borg_bool
+    name = borg_name(name)  
   end
-  return @name
+  name.join(' ')
 end
 
-puts "New name: #{modified_name(false)}"
-puts "New borg name: #{modified_name(true)}"
+def reverse_name(name)
+  last_first = name.split.reverse
+end
+
+def borg_name(name)
+    last_first_borg = name << "Borg"
+end
+
+name = "Johanna Jackson"
+
+puts "New name: #{mangle_name(name, false)}"
+puts "New borg name: #{mangle_name(name, true)}"
